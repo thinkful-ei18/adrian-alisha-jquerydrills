b04d8a0f248eb7cd76e564fcc818a93eee7b650b
Adrian Ross <adriantoddross@gmail.com>
2018-01-18 17:59:55 -0500

    added incomplete fizzbuzz drill

---
+++ fizzbuzz-forreal/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+$('#number-chooser').submit(function (event) {
+  event.preventDefault();
+  // variable that holds the numbers from user
+  let userInput = $('#number-choice').val();
+  console.log(userInput); // userinput tester
+
+
+}); // this closes line 3!
+
+function generateFizzBuzz (num) {
+  // this function does one thing and one thing only: a pure function! 
+  if (num % 3 === 0) {
+    return 'fizz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } else if (num % 15 === 0) {
+    return 'fizzbuzz';
+  } else {
+    return num;
+  }
+}
+
+console.log(generateFizzBuzz(10)); // fizzbuzz tester
