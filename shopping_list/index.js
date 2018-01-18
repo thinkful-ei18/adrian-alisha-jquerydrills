994d37719a95ce68455e050c6a8451eedc4beca8
alishaevn <alieva.inc@gmail.com>
2018-01-18 13:50:40 -0800

    completed shopping list app

--- shopping_list/index.js
+++ shopping_list/index.js
@@ -10,19 +10,22 @@ Using this and event delegation
 Requirements
 In terms of user experience, your shopping list app must allow users to:
 
-  1. enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
-  2. check and uncheck items on the list by clicking the "Check" button
-  3. permanently remove items from the list
+  ONE. enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+  TWO. check and uncheck items on the list by clicking the "Check" button
+  THREE. permanently remove items from the list
 
   Additionally:
 
 You must use a CDN-hosted version of jQuery *
 Put your application code in a file called index.js and link to it in index.html *
 Be sure to put both script elements at the bottom of the <body> element. *
-Do not alter index.html or main.css other than adding the links to the external JavaScript inside index.html. Write JavaScript code that works with the existing HTML and CSS to implement the required features.
+Do not alter index.html or main.css other than adding the links to the external JavaScript inside index.html. Write JavaScript code that works with the existing HTML and CSS to implement the required features. *
 Hint: you may find it helpful to read up on and use the following jQuery methods: .submit(), preventDefault(), toggleClass(), and closest().
 */
 
+
+
+// ONE
 // listen for the button to be clicked *
 // when clicked find the text in the input field *
 // add the chosen text to the list *
@@ -43,8 +46,30 @@ $('#js-shopping-list-form').submit(function (event) {
       </li>`);
 });
 
-// $('.shopping-item-toggle').on('click').toggleClass('.shopping-item__checked');
 
+
+// TWO
 $('.shopping-list').on('click', '.shopping-item-toggle', function (event) {
   $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+});
+
+/*
+$('.shopping-list') // look out for something with this class
+.on('click', // when you click something with the class above...
+'.shopping-item-toggle', // look for an element with this class that's INSIDE the shopping-list class.
+function (event) { // now do this
+  $(this) // this = event.currentTarget. SO, listen up for the event.currentTarget
+  .closest // find the closest PARENT element with the following characteristics
+  ('li') // characteristics = <li>
+  .find // find the closest CHILD element with the following characteristics
+  ('.shopping-item') // characteristics = shopping-item class
+  .toggleClass('shopping-item__checked'); // add the class inside the ()
+});
+*/
+
+
+
+// THREE
+$('.shopping-list').on('click', '.shopping-item-delete', function (event) {
+  $(this).closest('li').remove();
 });
